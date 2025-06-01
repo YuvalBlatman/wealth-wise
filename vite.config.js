13691e0aba36b190c69965aee4375b110e00bbea
ron gaist <ron0526634282@gmail.com>
2025-06-01 17:52:28 +0300

    return to normal

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dashboard',
+  base: '/wealth-wise/',
   plugins: [react()],
   server: {
     allowedHosts: true
